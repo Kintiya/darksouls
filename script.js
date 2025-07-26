dac5e7e0144b953ba2ccd5271acb6c8c2e50620c
Kintiya <kintiya.silchenko@gmail.com>
2025-07-26 21:26:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function triggerFlash(event) {
   event.preventDefault();
-  const flash = document.getElementById('flash');
-  flash.style.opacity = 1;
-
+  const flash = document.getElementById("flash");
+  flash.style.opacity = "1";
+  flash.style.transition = "opacity 0.5s";
+  setTimeout(() => {
+    flash.style.opacity = "0";
+  }, 400);
   setTimeout(() => {
     window.location.href = event.target.href;
-  }, 600);
+  }, 500);
 }
